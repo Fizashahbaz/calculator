3c48123f4b4d30286cf783f225c369b61ce2df84
fiza <sfiza5650@gmail.com>
2025-11-30 21:06:33 +0500

    commit initial files

---
+++ app.js
@@ -0,0 +1,32 @@
+
+let result = document.getElementById("result");
+let buttons = document.querySelectorAll("button");
+
+let input = "";
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+
+        let value = button.textContent;  
+
+        if (value === "C") {
+            input = "";
+            result.value = "";
+        }
+
+        else if (value === "=") {
+            try {
+                input = eval(input);
+                result.value = input;
+            } catch (error) {
+                result.value = "Error";
+            }
+        }
+
+        else {
+            input += value;
+            result.value = input;
+        }
+
+    });
+});
